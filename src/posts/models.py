81c81960551347931c52c9e362a7d1ca664cf8b4
SirawichDev <sirawit0676@gmail.com>
2018-05-01 23:41:07 +0700

    return title

--- src/posts/models.py
+++ src/posts/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
     Comment = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=Falsem auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
