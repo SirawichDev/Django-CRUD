f30069d035adacc1d57b19621cb1b5e6bd709845
SirawichDev <sirawit0676@gmail.com>
2018-05-01 23:37:21 +0700

    Title Comment Datetime update

--- src/posts/models.py
+++ src/posts/models.py
@@ -5,4 +5,4 @@ from __future__ import unicode_literals
 class Post(models.Model):
     title = models.CharField()
     Comment = models.TextField()
-    
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
