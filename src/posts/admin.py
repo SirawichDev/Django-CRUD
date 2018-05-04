af557f2924cfee0d457ce99d421c20930be598d4
SirawichDev <sirawit0676@gmail.com>
2018-05-04 23:14:50 +0700

    search bar

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -5,7 +5,8 @@ from .models import Post
 class IncreatOptionAdmin(admin.ModelAdmin):
     list_display = ["title","Comment","timestamp","updated"]
     list_display_links = ["updated","title"]
-    list_filter = ["title"]
+    list_filter = ["title","updated"]
+    search_fields = ["title"]
     class Meta:
         model = Post
 
