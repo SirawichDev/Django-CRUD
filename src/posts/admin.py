23da11e7bc6e32a3d479f70d292f74cf9917e4df
SirawichDev <sirawit0676@gmail.com>
2018-05-04 23:19:17 +0700

    fixed title can't in list_editable and list_display_links together

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -5,7 +5,7 @@ from .models import Post
 class IncreatOptionAdmin(admin.ModelAdmin):
     list_display = ["title","Comment","timestamp","updated"]
     list_editable = ["title"]
-    list_display_links = ["updated","title"]
+    list_display_links = ["updated"]
     list_filter = ["title","updated"]
     search_fields = ["title"]
     class Meta:
