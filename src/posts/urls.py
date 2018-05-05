1cc70c9dc241d790719c27077eca6053500c9434
SirawichDev <sirawit0676@gmail.com>
2018-05-05 08:42:53 +0700

    new path create, detail,update,delete

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import url
-from posts.views import go_to_home
+from . import views
 urlpatterns = [
-    url(r'^$', go_to_home,name="home"),
+    url(r'^$', views.go_to_list,name="list"),
+
+    url(r'^create/$', views.go_to_create,name="create"),
+
+    url(r'^detail/$', views.go_to_detail,name="detail"),
+
+    url(r'^update/$', views.go_to_update,name="update"),
+
+    url(r'^delete/$', views.go_to_delete,name="delete"),
     #  url(r'^post/$', "posts.views.posts_home"),
 ]
