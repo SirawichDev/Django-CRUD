6559c6ff1139941ba9cb8ea6e3633c3c583b4971
SirawichDev <sirawit0676@gmail.com>
2018-05-05 09:40:38 +0700

    fix error add ',' in urls

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import url
 from .views import (
-    go_to_list
-    go_to_create
-    go_to_detail
-    go_to_update
-    go_to_delete
+    go_to_list,
+    go_to_create,
+    go_to_detail,
+    go_to_update,
+    go_to_delete,
 )
 urlpatterns = [
     url(r'^$', go_to_list),
 
-    url(r'^create/$',   go_to_create),
+    url(r'^create/$', go_to_create),
 
     url(r'^detail/$',    go_to_detail),
 
